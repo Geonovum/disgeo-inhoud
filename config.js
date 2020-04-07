41a23bd1a5267be132ed4833284b5f46b8b4bdef
Linda van den Brink <l.vandenbrink@geonovum.nl>
2020-04-07 22:01:00 +0200

    update config
    config voor volgende werkversie

--- config.js
+++ config.js
@@ -40,8 +40,8 @@ var respecConfig =
 {
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
   //specStatus: "GN-BASIS",             // Basis Document
-  //specStatus: "GN-WV",              // Werk Versie
-  specStatus: "GN-CV",              // Consultatie Versie
+  specStatus: "GN-WV",              // Werk Versie
+  //specStatus: "GN-CV",              // Consultatie Versie
   //specStatus: "GN-VV",              // Vaststellings Versie
   //specStatus: "GN-DEF",             // Definitieve Versie
   //-- specType is verplicht bij alle andere dan BASIS ---------------------------------
@@ -57,7 +57,7 @@ var respecConfig =
   format: "markdown",                 // altijd "markdown"     
   //-- publishDate is verplicht -------------------------------------------------------
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
-  publishDate: "2020-04-07",  	    // Format is "YYYY-MM-DD"
+  //publishDate: "2020-04-07",  	    // Format is "YYYY-MM-DD"
   //-- Repositorynaam op GitHub -------------------------------------------------------
   github: "https://github.com/geonovum/disgeo-inhoud",
   //-- Repositorynaam/issues op GitHub ------------------------------------------------
@@ -152,8 +152,8 @@ var respecConfig =
   postProcess:[custGHPG],   //-- Optioneel voor een multi document repository
 
   //-- Voor dit blok geldt: alleen als er eerdere versies zijn en altijd beiden aan/uit! 
-  //previousPublishDate: "2018-09-18",  	    // Format is "YYY-MM-DD"
-  //previousMaturity: "CV",                   // kies 1 van deze 2 regels  	  
+  previousPublishDate: "2020-04-07",  	    // Format is "YYY-MM-DD"
+  previousMaturity: "CV",                   // kies 1 van deze 2 regels  	  
   //previousMaturity: "VV",  	                // kies 1 van deze 2 regels
 
   //-- Optionele parameters:
