272962660047e066895315216757143d96a796ef
Linda van den Brink <l.vandenbrink@geonovum.nl>
2020-06-23 15:46:34 +0200

    Update config.js

--- config.js
+++ config.js
@@ -40,10 +40,10 @@ var respecConfig =
 {
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
   //specStatus: "GN-BASIS",             // Basis Document
-  //specStatus: "GN-WV",              // Werk Versie
+  specStatus: "GN-WV",              // Werk Versie
   //specStatus: "GN-CV",              // Consultatie Versie
   //specStatus: "GN-VV",              // Vaststellings Versie
-  specStatus: "GN-DEF",             // Definitieve Versie
+  //specStatus: "GN-DEF",             // Definitieve Versie
   //-- specType is verplicht bij alle andere dan BASIS ---------------------------------
   //specType: "NO",                   // Norm
   //specType: "ST",                   // Standaard
@@ -58,7 +58,7 @@ var respecConfig =
   //-- publishDate is verplicht -------------------------------------------------------
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
   //publishDate: "2020-04-07",  	    // Format is "YYYY-MM-DD"
-  publishDate: "2020-06-17",  	    // Format is "YYY-MM-DD"
+  //publishDate: "2020-06-17",  	    // Format is "YYY-MM-DD"
   //-- Repositorynaam op GitHub -------------------------------------------------------
   github: "https://github.com/geonovum/disgeo-inhoud",
   //-- Repositorynaam/issues op GitHub ------------------------------------------------
@@ -153,8 +153,8 @@ var respecConfig =
   postProcess:[custGHPG],   //-- Optioneel voor een multi document repository
 
   //-- Voor dit blok geldt: alleen als er eerdere versies zijn en altijd beiden aan/uit! 
-  previousPublishDate: "2020-04-10",  	    // Format is "YYY-MM-DD"
-  previousMaturity: "GN-CV",                   // kies 1 van deze 2 regels  	  
+  previousPublishDate: "2020-06-17",  	    // Format is "YYY-MM-DD"
+  previousMaturity: "GN-DEF",                   // kies 1 van deze 2 regels  	  
   //previousMaturity: "GN-VV",  	                // kies 1 van deze 2 regels
 
   //-- Optionele parameters:
